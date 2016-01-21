bd5a793388cf50879030bd9108f4188b5645d407
reinaldo <reinaldojun@gmail.com>
2016-01-21 18:15:09 -0300

    Renomear libs

--- scriptLoadJboss.sh
+++ scriptLoadJboss.sh
@@ -1,7 +1,7 @@
-cp -R $GSAN_PATH/lib/jboss-config/jboss-hibernate/*.jar $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/
-cp -R $GSAN_PATH/lib/jboss-config/jboss-web/*.jar $JBOSS_HOME/server/default/deploy/jbossweb-tomcat50.sar/
-cp -R $GSAN_PATH/lib/jboss-config/postgres/*.jar $JBOSS_HOME/server/default/lib/
-cp -R $GSAN_PATH/lib/jboss-config/mondrian.war $JBOSS_HOME/server/default/deploy/mondrian.war
+cp -R $JBOSS_LIBS/jboss-hibernate/*.jar $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/
+cp -R $JBOSS_LIBS/jboss-web/*.jar $JBOSS_HOME/server/default/deploy/jbossweb-tomcat50.sar/
+cp -R $JBOSS_LIBS/postgres/*.jar $JBOSS_HOME/server/default/lib/
+cp -R $JBOSS_LIBS/mondrian.war $JBOSS_HOME/server/default/deploy/mondrian.war
 rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/cglib-full-2.0.1.jar
 rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/hibernate2.jar
 
