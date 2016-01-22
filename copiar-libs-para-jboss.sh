586a5de61597f361312d5ee453a8ed702f38e3da
Luiz Sanches <luizgrsanches@gmail.com>
2016-01-22 02:22:20 -0300

    copiar dtd para jboss home

--- copiar-libs-para-jboss.sh
+++ copiar-libs-para-jboss.sh
@@ -11,3 +11,5 @@ cp -v ./scripts/jboss_init_debian.sh $JBOSS_HOME/bin/
 
 rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/cglib-full-2.0.1.jar
 rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/hibernate2.jar
+
+cp -v ./docs/dtd/* "$JBOSS_HOME/docs/dtd"
