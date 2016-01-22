09323dfc1e356f6e70e7c5c4441d381499d6df36
Luiz Sanches <luizgrsanches@gmail.com>
2016-01-21 21:55:21 -0300

    correcao caso na exista a variavel de ambiente

---
+++ copiar-libs-para-jboss.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[ -z "$JBOSS_HOME" ] && JBOSS_HOME="/opt/jboss"
+
+cp -vR ./jboss-hibernate/*.jar $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/
+cp -vR ./jboss-web/*.jar $JBOSS_HOME/server/default/deploy/jbossweb-tomcat50.sar/
+cp -vR ./postgres/*.jar $JBOSS_HOME/server/default/lib/
+cp -vR ./mondrian.war $JBOSS_HOME/server/default/deploy/
+rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/cglib-full-2.0.1.jar
+rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/hibernate2.jar
--- scriptLoadJboss.sh
+++
@@ -1,7 +0,0 @@
-cp -R $JBOSS_LIBS/jboss-hibernate/*.jar $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/
-cp -R $JBOSS_LIBS/jboss-web/*.jar $JBOSS_HOME/server/default/deploy/jbossweb-tomcat50.sar/
-cp -R $JBOSS_LIBS/postgres/*.jar $JBOSS_HOME/server/default/lib/
-cp -R $JBOSS_LIBS/mondrian.war $JBOSS_HOME/server/default/deploy/mondrian.war
-rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/cglib-full-2.0.1.jar
-rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/hibernate2.jar
-
