6903bd476ccfd58659fa5b4923af6ba2a4bcfbd6
Luiz Sanches <luizgrsanches@gmail.com>
2016-01-21 22:22:03 -0300

    copiar script de inicializacao do jboss

--- copiar-libs-para-jboss.sh
+++ copiar-libs-para-jboss.sh
@@ -6,5 +6,8 @@ cp -vR ./jboss-hibernate/*.jar $JBOSS_HOME/server/default/deploy/jboss-hibernate
 cp -vR ./jboss-web/*.jar $JBOSS_HOME/server/default/deploy/jbossweb-tomcat50.sar/
 cp -vR ./postgres/*.jar $JBOSS_HOME/server/default/lib/
 cp -vR ./mondrian.war $JBOSS_HOME/server/default/deploy/
+
+cp -v ./scripts/jboss_init_debian.sh $JBOSS_HOME/bin/
+
 rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/cglib-full-2.0.1.jar
 rm $JBOSS_HOME/server/default/deploy/jboss-hibernate.deployer/hibernate2.jar
